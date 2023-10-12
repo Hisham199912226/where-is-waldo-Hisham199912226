57b12a9d519b33004fa1f0f8496966a6a62cbf3c
Hisham Al-Eesa <hisham.aleesa@gmail.com>
2023-10-12 19:44:40 +0300

    Cleaning the code and some optimization

--- index.js
+++ index.js
@@ -16,22 +16,24 @@ const example2 = [["O", "O", "O", "O"],
   ["O", "O", "O", "O"]];
 
 const whereIsWaldo = (matrix) => {
-  let possibleWildoIndex;
+  let possibleWaldoIndex;
   for(let row = 0 ; row < matrix.length; row++){
     for(let column = 0 ; column < matrix[row].length - 1; column++){
-      if(matrix[row][column] != matrix[row][column + 1])
-         possibleWildoIndex = [row, column];
+      if(matrix[row][column] !== matrix[row][column + 1]){
+        possibleWaldoIndex = [row, column];
+         break;
+      }
     }
   }
 
-  if(checkNotWeldo(matrix,possibleWildoIndex))
-    return [possibleWildoIndex[0] + 1, possibleWildoIndex[1] + 1 + 1];
+  if(checkNotWeldo(matrix,possibleWaldoIndex))
+    return [possibleWaldoIndex[0] + 1, possibleWaldoIndex[1] + 1 + 1];
   
-  return [possibleWildoIndex[0] + 1, possibleWildoIndex[1] + 1];
+  return [possibleWaldoIndex[0] + 1, possibleWaldoIndex[1] + 1];
 }
 
-function checkNotWeldo(matrix, possibleWildoIndex){
-  return matrix.flat().filter(element => element === matrix[possibleWildoIndex[0]][possibleWildoIndex[1]]).length > 1;
+function checkNotWeldo(matrix, possibleWaldoIndex){
+  return matrix.flat().filter(element => element === matrix[possibleWaldoIndex[0]][possibleWaldoIndex[1]]).length > 1;
 }
 
 console.log("Expect => [ 3, 2 ]");
